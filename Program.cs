a700b6764943d023d317a5d90e01da5f5c9c9b76
Alfredo Cezar Benedetti <alfredo.c-benedetti@outlook.com>
2024-04-23 21:13:20 -0700

    Adicionado arquivo readme.md

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new("TV", 500.00, 10);
+        }
+    }
+}
